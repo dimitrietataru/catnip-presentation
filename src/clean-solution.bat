a610836ac6c9787b64925e29cf501d4045ad688a
Dimitrie Tataru <dimitriettr@gmail.com>
2024-10-28 19:31:36 +0200

    Update the 'clean-solution' script

--- src/clean-solution.bat
+++ src/clean-solution.bat
@@ -1,7 +1,16 @@
 rmdir /s /q .\CatNip.Presentation\bin
 rmdir /s /q .\CatNip.Presentation\obj
 
+rmdir /s /q .\CatNip.Presentation.IntegrationTest\bin
+rmdir /s /q .\CatNip.Presentation.IntegrationTest\obj
+
+rmdir /s /q .\CatNip.Presentation.IntegrationTest.XUnit\bin
+rmdir /s /q .\CatNip.Presentation.IntegrationTest.XUnit\obj
+
 rmdir /s /q .\CatNip.Presentation.Test\bin
 rmdir /s /q .\CatNip.Presentation.Test\obj
+
+rmdir /s /q .\CatNip.Presentation.Test.XUnit\bin
+rmdir /s /q .\CatNip.Presentation.Test.XUnit\obj
 
 pause
